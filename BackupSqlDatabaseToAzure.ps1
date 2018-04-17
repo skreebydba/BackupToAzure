87a051b63f6d976302497b520c2f65156258abf9
skreebydba <skreebydba@users.noreply.github.com>
2018-04-17 11:10:26 -0500

    Initial add of logic

--- BackupSqlDatabaseToAzure.ps1
+++ BackupSqlDatabaseToAzure.ps1
@@ -1,17 +1,16 @@
-﻿function Do-Something {
+﻿function Backup-SqlDatabaseToAzure {
   <#
   .SYNOPSIS
-  Describe the function here
+  Backup one or more databases from a SQL Server instance to Azure Blob Storage
   .DESCRIPTION
-  Describe the function in more detail
+  The function takes a SQL Server instance as input and displays a grid of all of the databases on that instance.  The user selects the databases to be backed up and the
+  function loops through the databases selected and backs them up to the Azure Blob Storage container specified.
   .EXAMPLE
-  Give an example of how to use it
-  .EXAMPLE
-  Give another example of how to use it
-  .PARAMETER computername
-  The computer name to query. Just one.
-  .PARAMETER logname
-  The name of a file to write failed computer names to. Defaults to errors.txt.
+  Backup-SqlDatabaseToAzure -instance "cncybook82\dev2017" -container "https://fbgazure.blob.core.windows.net/fbgazurecont" -credential "BackupToAzure";
+  .PARAMETER instance
+  The SQL Server instance to back up from. Just one.
+  .PARAMETER container
+  The Azure Blob Storage container to backup to.
   #>
   [CmdletBinding(SupportsShouldProcess=$True,ConfirmImpact='Low')]
   param
@@ -19,28 +18,46 @@
     [Parameter(Mandatory=$True,
     ValueFromPipeline=$True,
     ValueFromPipelineByPropertyName=$True,
-      HelpMessage='What computer name would you like to target?')]
-    [Alias('host')]
-    [ValidateLength(3,30)]
-    [string[]]$computername,
+      HelpMessage='What SQL Server instance are you backing up from?')]
+    [Alias('sqlinst')]
+    [ValidateLength(3,255)]
+    [string]$instance,
+
+    [Parameter(Mandatory=$True,
+    ValueFromPipeline=$True,
+    ValueFromPipelineByPropertyName=$True,
+      HelpMessage='What Azure Blob Storage container are you backing up to?')]
+    [Alias('blob')]
+    [ValidateLength(3,255)]
+    [string]$container,
+
+    [Parameter(Mandatory=$True,
+    ValueFromPipeline=$True,
+    ValueFromPipelineByPropertyName=$True,
+      HelpMessage='What SQL Server credential are you using?')]
+    [Alias('cred')]
+    [ValidateLength(3,255)]
+    [string]$credential
 		
-    [string]$logname = 'errors.txt'
   )
 
-  begin {
-  write-verbose "Deleting $logname"
-    del $logname -ErrorActionSilentlyContinue
-  }
-
   process {
+            $date = Get-Date -Format yyyyMMdd_HHmmss;
+            $databases = Get-SqlDatabase -ServerInstance $instance | Out-GridView -PassThru | Select-Object -ExpandProperty Name;
 
-    write-verbose "Beginning process loop"
+            foreach($database in $databases)
+            {
+                $backupfile = "$container/$database`_$date.bak";
 
-    foreach ($computer in $computername) {
-      Write-Verbose "Processing $computer"
-      if ($pscmdlet.ShouldProcess($computer)) {
-        # use $computer here
-      }
+                #Write-Output $backupfile;
+                Write-Output "Backup starting for " $database;
+
+                Backup-SqlDatabase -ServerInstance $instance `
+                -Database $database `
+                -BackupAction Database `
+                -BackupFile $backupfile `
+                -SqlCredential $credential `
+                -CompressionOption On;
+            }
     }
-  }
 }
