0697bb65b310bfb626cab083858c4d423e7b464b
skreebydba <skreebydba@users.noreply.github.com>
2018-04-17 16:12:04 -0500

    Initial Commit

---
+++ DeleteBlobFiles.ps1
@@ -0,0 +1,17 @@
+﻿#Login-AzureRmAccount;
+
+$StorageAccountName = "fbgazure"; 
+$StorageAccountKey = "+i+72Kn5blMqIg/ic5FJ/El1NahGiF9Frhtd98Iu8J5GKXtGrgjXNIrsEuRWgseUYCtgHtC9jt/uDLGw095ltw==";
+
+$ctx = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey;
+
+$ContainerName = “fbgazurecont”;
+
+$blobs = Get-AzureStorageBlob -Container $ContainerName -Context $ctx | Select -ExpandProperty Name ;
+
+$blobs; 
+
+foreach($blob in $blobs)
+{
+    Remove-AzureStorageBlob -Blob $Blob -Container $ContainerName -Context $ctx;
+}
---
+++ ExecBackupToAzure.ps1
@@ -0,0 +1,9 @@
+﻿$credential = "BackupToAzure";
+$instance = "cncybook82\dev2017";
+$backuppath = "https://fbgazure.blob.core.windows.net/fbgazurecont";
+
+Backup-SqlDatabaseToAzure -instance $instance -container $backuppath -credential $credential;
+
+#Get-SqlDatabase -ServerInstance $instance | Select-Object -ExpandProperty Name | Where-Object {$_.Name -ne "tempdb"};
+
+ 
