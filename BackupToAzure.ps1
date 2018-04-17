9a521898b18ee67359f2e9298cc97cb0d0cb40d9
skreebydba <skreebydba@users.noreply.github.com>
2018-04-16 23:36:03 -0500

    Inital Checkin

---
+++ BackupToAzure.ps1
@@ -0,0 +1,22 @@
+﻿$date = Get-Date -Format yyyyMMdd_HHmmss;
+$credential = "BackupToAzure";
+$instance = "cncybook82\dev2017";
+$backuppath = "https://fbgazure.blob.core.windows.net/fbgazurecont";
+
+$databases = (Get-SqlDatabase -ServerInstance $instance | Out-GridView -PassThru | Select-Object -Property Name).Name;
+
+foreach($database in $databases)
+{
+    $backupfile = "$backuppath/$database`_$date.bak";
+
+    #Write-Output $backupfile;
+    Write-Output "Backup starting for " $database;
+
+    Backup-SqlDatabase -ServerInstance $instance `
+    -Database $database `
+    -BackupAction Database `
+    -BackupFile $backupfile `
+    -SqlCredential $credential `
+    -CompressionOption On;
+}
+
