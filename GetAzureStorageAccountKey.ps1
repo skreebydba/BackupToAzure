c50a8d9010e3c521e286c438c5a47c25e2d286ad
skreebydba <skreebydba@users.noreply.github.com>
2018-04-18 13:22:36 -0500

    Initial Commit

---
+++ GetAzureStorageAccountKey.ps1
@@ -0,0 +1,13 @@
+﻿#Connect-AzureRmAccount;
+Set-Location SQLSERVER:\SQL\cncybook82\dev2017;
+
+$rgname = 'fbgazurerg';
+$storagename = 'fbgazure';
+$credname = 'TestPsCred2'
+
+$key = Get-AzureRmStorageAccountKey -ResourceGroupName $rgname -Name $storagename | Where-Object {$_.KeyName -eq 'key2'} | Select-Object -ExpandProperty Value;
+
+
+$secretstring = ConvertTo-SecureString $key -AsPlainText -Force;
+
+New-SqlCredential -Name $credname -Identity $storagename -Secret $secretstring;
