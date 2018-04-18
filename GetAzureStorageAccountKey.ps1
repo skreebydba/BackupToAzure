342bc1b0fc472e5e04e866d72fa835e72c639cc2
skreebydba <skreebydba@users.noreply.github.com>
2018-04-18 13:31:19 -0500

    Updated key value to variable

--- GetAzureStorageAccountKey.ps1
+++ GetAzureStorageAccountKey.ps1
@@ -3,9 +3,10 @@ Set-Location SQLSERVER:\SQL\cncybook82\dev2017;
 
 $rgname = 'fbgazurerg';
 $storagename = 'fbgazure';
-$credname = 'TestPsCred2'
+$credname = 'TestPsCred3';
+$keynum = 'key1'
 
-$key = Get-AzureRmStorageAccountKey -ResourceGroupName $rgname -Name $storagename | Where-Object {$_.KeyName -eq 'key2'} | Select-Object -ExpandProperty Value;
+$key = Get-AzureRmStorageAccountKey -ResourceGroupName $rgname -Name $storagename | Where-Object {$_.KeyName -eq $keynum} | Select-Object -ExpandProperty Value;
 
 
 $secretstring = ConvertTo-SecureString $key -AsPlainText -Force;
